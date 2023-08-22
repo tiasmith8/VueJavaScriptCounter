0ac65e0627125ccb7a9e59f379db67f532d8e35e
Tia Smith <tsmith@onosys.com>
2023-08-22 15:42:24 -0400

    initial

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <div>
+    <h4>
+      The current count is...
+    </h4>
+    <h1>{{ count }}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  const count = ref(0);
+</script>
+
+<style scoped> 
+  main {
+    background-color: black;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  div {
+    text-align: center;
+  }
+
+  h4, h1 {
+    color: orange;
+  }
+</style>
